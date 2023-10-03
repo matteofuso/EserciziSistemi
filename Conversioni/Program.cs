e041f0eceed2442b234969a6eadeb14685cc0601
Matteo Fuso <matteo.fuso@iisviolamarchesini.edu.it>
2023-10-03 16:02:12 +0200

    Metodo Convert_Binario_To_Decimale_Puntato()

--- Conversioni/Program.cs
+++ Conversioni/Program.cs
@@ -10,6 +10,7 @@ namespace Conversioni
             int[] dp;
             InserimentoB(b);
             Console.WriteLine("Il numero intero è {0}", Convert_Binario_To_Decimale(b));
+            Console.WriteLine("Il numero decimale puntato è: {0}", string.Join(".", Convert_Binario_To_Decimale_Puntato(b)));
             InserimentoDP(out dp);
             Console.WriteLine("Il numero intero è {0}", Convert_Decimale_Puntato_To_Decimale(dp));
             Console.ReadLine();
@@ -43,21 +44,43 @@ namespace Conversioni
         }
         static int Convert_Binario_To_Decimale(bool[] b)
         {
-            int intero = 0;
+            int intero = 0, len = b.Length - 1;
             for (int i = 0; i < b.Length; i++)
             {
-                intero += Convert.ToInt32(b[31 - i]) * (int)Math.Pow(2, i);
+                if (b[i])
+                {
+                    intero += (int)Math.Pow(2, len - i);
+                }
             }
             return intero;
         }
         static int Convert_Decimale_Puntato_To_Decimale(int[] dp)
         {
-            int intero = 0;
+            int intero = 0, len = dp.Length - 1;
             for (int i = 0; i < dp.Length; i++)
             {
-                intero += dp[3 - i] * (int)Math.Pow(256, i);
+                intero += dp[len - i] * (int)Math.Pow(256, i);
             }
             return intero;
+        }
+        static int[] Convert_Binario_To_Decimale_Puntato(bool[] b)
+        {
+            int[] dp = new int[4];
+            int intero, j = 0, next;
+            for (int i = 0; i < dp.Length; i++)
+            {
+                intero = 0;
+                next = 8 * (i + 1);
+                for (; j < next; j++)
+                {
+                    if (b[j])
+                    {
+                        intero += (int)Math.Pow(2, next - j - 1);
+                    }
+                }
+                dp[i] = intero;
+            }
+            return dp;
         }
     }
 }
